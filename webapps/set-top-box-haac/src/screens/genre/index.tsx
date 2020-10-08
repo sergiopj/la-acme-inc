b78d95c28f38fef106f929f4dc82fce64acb8cda
David Cantelar <david.cantelar@telefonica.com>
2020-10-08 10:11:38 +0200

    feat: navigable catalog items

--- webapps/set-top-box-haac/src/screens/genre/index.tsx
+++ webapps/set-top-box-haac/src/screens/genre/index.tsx
@@ -1,7 +1,7 @@
 import './genre.scss';
 
-import React, { useRef } from 'react';
-import { screenReady, NavigableButton, Footer } from '@telefonica/la-web-sdk';
+import React, { useCallback, useRef } from 'react';
+import { screenReady, NavigableButton, Footer, NavigableWrapper } from '@telefonica/la-web-sdk';
 import { Intent, Game, GameScreenData } from '../../../../../dialogs/src/models';
 import { useAura } from '@telefonica/la-web-sdk';
 
@@ -21,28 +21,48 @@ const GameScreen: React.FC<GameScreenData> = (gameData: GameScreenData) => {
         return `${process.env.PUBLIC_URL}/assets/imgs/notfound.png`;
     };
 
+    const onFocus = useCallback((event: Event) => {
+
+        console.log('focus', event);
+        const element = event.target as HTMLElement;
+        element.scrollTo({ behavior: 'smooth' });
+    }, []);
     return (
         <div className="genre-screen">
             <h1 className="title">GAMES - CATEGORY {gameData.title.toUpperCase()} </h1>
             <div className="games">
-                {gameList.current.map((game: Game) => (
-                    <div className="game" key={game.id}>
-                        <div className="upper-container">
-                            <img src={getImage(game.img)} alt={game.title} />
-                            <div className="right-container">
-                                <b>Name: {game.title}</b>
-                                <p>Platform: {game.platform}</p>
-                                <p>Year: {game.year}</p>
+                {gameList.current.map((game: Game, index: number) => (
+                    <NavigableWrapper
+                        id={game.id}
+                        key={game.id}
+                        autoScroll={true}
+                        focusedClass="game__focused"
+                        defaultFocused={index === 0}
+                        onFocus={onFocus}
+                    >
+                        <div className="game">
+                            <div className="upper-container">
+                                <img src={getImage(game.img)} alt={game.title} />
+                                <div className="right-container">
+                                    <b>Name: {game.title}</b>
+                                    <p>Platform: {game.platform}</p>
+                                    <p>Year: {game.year}</p>
+                                </div>
+                            </div>
+                            <div className="lower-container">
+                                <p>Description: {game.description}</p>
                             </div>
                         </div>
-                        <div className="lower-container">
-                            <p>Description: {game.description}</p>
-                        </div>
-                    </div>
+                    </NavigableWrapper>
                 ))}
             </div>
             <Footer>
-                <NavigableButton onClick={() => goToHome()} defaultClass="" focusedClass="" id="categories-button">
+                <NavigableButton
+                    id="categories-button"
+                    defaultClass="button"
+                    focusedClass="button focus"
+                    onClick={() => goToHome()}
+                >
                     BACK
                 </NavigableButton>
             </Footer>
